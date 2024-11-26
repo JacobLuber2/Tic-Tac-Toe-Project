55cf197e72003fd3b14b578445a364255a1e38de
JacobLuber2 <jaluber10@gmail.com>
2024-11-26 15:26:35 -0700

    fixed minor layout issue, added README.md file

--- index.js
+++ index.js
@@ -1,145 +1,151 @@
 let tableCells = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((element) => {
-  return document.getElementById(element.toString());
-});
-let board = document.getElementById("board");
+    return document.getElementById(element.toString());
+}); // visible gameboard
+let board = document.getElementById("board"); // board container
 board.style.display = "none";
+// buttons for players to submit their names'
 let player1Submit = document.getElementById("submitplayer1");
 let player2Submit = document.getElementById("submitplayer2");
+// input + button containers
 let player1div = document.getElementById("player1div");
 let player2div = document.getElementById("player2div");
+// hide board screen
 player2div.style.display = "none";
-player2Submit.style.display = "none"
+player2Submit.style.display = "none";
+// set game values'
 let currentPlayer = "X";
 let player1 = "";
 let player2 = "";
-let gameOver = false;
+let gameOver = false; /* reverts to true in the event of tie or winner then back to false upon resetting board */
 let resetBtn = document.getElementById("reset");
 resetBtn.style.display = "none";
 function checkPlayers() {
-  if (player1 && player2) {
-    board.style.display = "block";
-    resetBtn.style.display = "block";
-  }
+    if (player1 && player2) {
+        // shows game screen
+        board.style.display = "inline";
+        resetBtn.style.display = "inline";
+    }
 }
+// event listeners when the username inputs are submitted
 player1Submit.addEventListener("click", () => {
-  let player1Input = document.getElementById("player1").value;
-  if (player1Input.trim() === "") {
-    window.alert("Username must have text, player1");
-  } else {
-  player1 = document.getElementById("player1").value;
-  checkPlayers();
-  player1Submit.remove();
-  player1div.remove();
-  player2div.style.display = "block";
-player2Submit.style.display = "block";
-  }
+    let player1Input = document.getElementById("player1").value;
+    if (player1Input.trim() === "") {
+        window.alert("Username must have text, player1");
+    } else {
+        // player 1 screen
+        player1 = document.getElementById("player1").value;
+        checkPlayers();
+        player1Submit.remove();
+        player1div.remove();
+        player2div.style.display = "inline";
+        player2Submit.style.display = "inline";
+    }
 });
 player2Submit.addEventListener("click", () => {
-  let player2Input = document.getElementById("player2").value;
-  if (player2Input.trim() === "") {
-    window.alert("Username must have text, player2");
-  } else {
-  player2 = document.getElementById("player2").value;
-  checkPlayers();
-  player2Submit.remove();
-  player2div.remove();
-  }
+    let player2Input = document.getElementById("player2").value;
+    if (player2Input.trim() === "") {
+        window.alert("Username must have text, player2");
+    } else {
+        // player 2 screen
+        player2 = document.getElementById("player2").value;
+        checkPlayers();
+        player2Submit.remove();
+        player2div.remove();
+    }
 });
 function changePlayer() {
-  if (currentPlayer === "X") {
-    currentPlayer = "O";
-  } else {
-    currentPlayer = "X";
-  }
-}
-tableCells.map((element) => {
-  element.addEventListener("click", (event) => {
-    if (gameOver == false) {
-    if (element.textContent === "") {
-      element.textContent = currentPlayer;
-      fillBoard(event.target.id);
-      checkWinner();
-      changePlayer();
+    if (currentPlayer === "X") {
+        currentPlayer = "O";
+    } else {
+        currentPlayer = "X";
     }
-  }
-  });
+}
+// sets text contents of the visible game board
+tableCells.map((element) => {
+    element.addEventListener("click", (event) => {
+        if (gameOver == false) {
+            if (element.textContent === "") {
+                element.textContent = currentPlayer;
+                fillBoard(event.target.id);
+                checkWinner();
+                changePlayer();
+            }
+        }
+    });
 });
-let gameBoard = ["", "", "", "", "", "", "", "", ""];
+let gameBoard = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]; /* system's gameboard, hidden */
 function fillBoard(id) {
-  gameBoard[id - 1] = currentPlayer;
+    gameBoard[id - 1] = currentPlayer;
 }
 const winCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ];
 function checkWinner() {
-  let winnerFound = false;
-  for (let i = 0; winCombinations.length > i; i++) {
-    let combination = winCombinations[i];
-    if (
-      gameBoard[combination[0]] == gameBoard[combination[1]] &&
-      gameBoard[combination[0]] == gameBoard[combination[2]] &&
-      gameBoard[combination[0]] != ""
-    ) {
-      declareWinner();
-      gameOver = true;
-      winnerFound = true;
+    let winnerFound = false;
+    for (let i = 0; winCombinations.length > i; i++) {
+        // loops over winning combinations
+        let combination = winCombinations[i];
+        if (
+            gameBoard[combination[0]] == gameBoard[combination[1]] &&
+            gameBoard[combination[0]] == gameBoard[combination[2]] &&
+            gameBoard[combination[0]] != ""
+            //  checks for matches against winCombinations array
+        ) {
+            declareWinner();
+            gameOver = true;
+            winnerFound = true;
+        }
     }
-  }
-  if (winnerFound != true) {
-    isTie();
-  }
-  if (winnerFound = true) {
-  for (let x = 0; x < tableCells.length; x++) {
-  if (tableCells[x].value = "") {
-  
-  }
-  }
-  }
+    if (winnerFound != true) {
+        // triggers in the event of a tie
+        isTie();
+    }
 }
 async function isTie() {
-  let filled = true;
-  for (let i = 0; i < 9; i++) {
-    if (gameBoard[i] == "") {
-      filled = false;
+    let filled = true;
+    for (let i = 0; i < 9; i++) {
+        if (gameBoard[i] == "") {
+            filled = false;
+        }
     }
-  }
-  if (filled == true) {
-    window.alert(`Neither ${player1} or ${player2} won`);
-    await waitToClearBoard();
-    resetBoard();
-  }
+    if (filled == true) {
+        window.alert(`Neither ${player1} or ${player2} won`);
+    }
 }
 async function declareWinner() {
-  if (currentPlayer === "X") {
-    window.alert(`${player1} WON`);
-    await waitToClearBoard();
-    resetBoard();
-  } else if (currentPlayer === "O") {
-    window.alert(`${player2} WON`);
-    await waitToClearBoard();
-    resetBoard();
-  }
+    if (currentPlayer === "X") {
+        window.alert(`${player1} WON`);
+    } else if (currentPlayer === "O") {
+        window.alert(`${player2} WON`);
+    }
 }
 resetBtn.addEventListener("click", () => {
-resetBoard();
+    resetBoard();
 });
 function resetBoard() {
-  gameBoard = ["", "", "", "", "", "", "", "", ""];
-  for (let index = 0; index < 9; index++) {
-    tableCells[index].textContent = "";
-  }
-  gameBoard = ["", "", "", "", "", "", "", "", ""];
-  gameOver = false;
-  currentPlayer = "X"
-}
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-const waitToClearBoard = async () => {
-await delay(2000);
+    // resets to starting game
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    for (let index = 0; index < 9; index++) {
+        tableCells[index].textContent = "";
+    }
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    gameOver = false;
+    currentPlayer = "X";
 }
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Tic-Tac-Toe
+
+## Usage:
+
+-You enter a name click submit button, repeat again  
+-Click a square  
+-If you have 3 of your symbols back to back in any order (row, coulumn, diagonal) You won!  
+-Click the reset board button to reset upon finishing the game or whenever else
+
+## Future Features:
+
+-**add more styling**  
+-**Congratulate the winner** -**online multiplayer**
+
+## Technologies used:
+
+-**HTML**  
+-**CSS**  
+-**JavaScript**  
+-**Git (for project management)**  
+-**VSCode (for codebase management)**  
+-**GitHub (for storing repository and hosting)**
+
+**Usage Instructions**
+
+1. Video demonstration at [Google Drive](https://drive.google.com/drive/folders/1WPtrEO1JeMeknNhpuNRsf7SndHTUCeuy)
+2. Or visit the live version of the app at [Library App](https://jacobluber2.github.io/Library-app/)
+3. Alternatively, Clone the repository using the following command:
+    ```bash
+    git clone git clone https://github.com/JacobLuber2/Tic-Tac-Toe-Project.git
+    ```
